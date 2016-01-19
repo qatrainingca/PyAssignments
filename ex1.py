d848badc7e5108dfb142e5e6063845d7e0643441
test <test@test.com>
2016-01-19 13:20:20 -0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print "Assignment for printing the max number of two numbers "
+a = input("Enter a value :")
+print "a value is :", a
+b = input("Enter b value :")
+print "b value is :", b
+if (a>b):
+    print "max value is a", a
+else:
+    print "max value is b", b
+print "End of assignment"
