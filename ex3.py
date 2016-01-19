2a328983547864601a2ed8dfadaca81baaf96863
test <test@test.com>
2016-01-19 13:33:30 -0800

    3rd commit

---
+++ ex3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+print "Assignment that computes length of given string"
+str1 = raw_input ("Enter any string :")
+print "Chars in string are :" ,len(str1)
+print " End of assignment"
