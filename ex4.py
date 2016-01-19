b8e321887889cb67ed6a62bec6bd68790c4b2a1f
test <test@test.com>
2016-01-19 13:34:57 -0800

    4th commit

---
+++ ex4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+print "Assignment of whether letter is vowel or not"
+a = raw_input("Enter any letter :")
+if(a == 'a' or a == 'e' or a == 'i' or a == 'o' or a == 'u' ):
+    print "VOWEL"
+else:
+    print "NON-VOWEl"
+print "End of assignment"
